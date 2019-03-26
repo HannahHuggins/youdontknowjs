7f925ea08d9401aa687dd3d188acb959df51d5b4
Hannah Huggins <dowdallhannah@yahoo.com>
2019-03-26 13:37:14 +0000

    notes on operators

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ a = b * 2
 /* This has 4 expressions in it:
 2 is a literal value expression
 b is a variable expression
-b * 2 is an airthmetic expression
+b * 2 is an arithmetic expression
 a = b * 2 is an assignment expression */ 
 
 b * 2 // this is an expression statement 
@@ -31,3 +31,60 @@ alert(a);
 // this is a call expression statement 
 
 //------------------------------------------------------------------------------------------------//
+
+// Output
+
+a = 21;
+
+b = a * 2;
+
+console.log(b);
+
+/* Here we are giving the variable a the value of 21, and then giving the answer to a * 2
+to variable b. The output is 42.
+Console.log prints the output. */
+
+//------------------------------------------------------------------------------------------------//
+
+// Prompts and alerts 
+
+age = prompt ("Please tell me your age:");
+
+console.log(age);
+
+// The age variable is whatever value you type into the console when the prompt asks 
+
+b = 2 + 3;
+
+alert(b)
+
+// This will cause a popup to display the value stored in b. 
+
+//------------------------------------------------------------------------------------------------//
+
+// Operators 
+
+* // multiplication
+= // Assignment (first calculate the value on the right hand side (Source value) of == and then put it into the variable on the left(target variable))
++ // Addition
+- // Subtraction
+/ // division 
++= // Compound assignment
+== // Equality == is loose equals, === is strict equals, != is loose not equals !== strict 
+< // less than (comparison)
+> //greater than (comparison)
+< = // less than or equals to (comparison)
+&& // and (logical)
+|| // or (logical)
+
+++ / -- // increment and decrement 
+var a = 20;
+
+a = a + 1;
+a = a * 2;
+// You should always declare the variable with var before using it. you only need to declare once for each scope 
+
+/* Objects are values that hold other values at specific named locations called properties. 
+*/
+//------------------------------------------------------------------------------------------------//
+
