a6ee1a247dc892977b38eb6453b259b0aaaae5ef
Hannah Huggins <dowdallhannah@yahoo.com>
2019-03-26 13:13:11 +0000

    Added notes about statements and expressions

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// A group of words, numbers and operators that performs a specific task is a statement. 
+a = b * 2;
+
+/* a and b are variables, which are boxes you can store your stuff in. 
+Variables hold values to be used by the program. 
+*/
