a307d8dcb954c5e1c5b95c4cae265b897c242c3f
Hannah Huggins <dowdallhannah@yahoo.com>
2019-03-26 13:25:53 +0000

    Added notes about output, prompts and alerts

--- scripts.js
+++ scripts.js
@@ -1,6 +1,33 @@
+// Statements, variables, literal values and operators. 
+
 // A group of words, numbers and operators that performs a specific task is a statement. 
+
 a = b * 2;
 
 /* a and b are variables, which are boxes you can store your stuff in. 
 Variables hold values to be used by the program. 
-*/
+The 2 is a literal value, it stands alone without being stored in a variable. 
+= and * are operators. */
+
+//------------------------------------------------------------------------------------------------//
+
+// Expressions 
+
+/* Statements are made up of one or more expressions. 
+An expression is any reference to a variable, value, set of variable, and or values
+combined with operators.*/
+
+a = b * 2 
+
+/* This has 4 expressions in it:
+2 is a literal value expression
+b is a variable expression
+b * 2 is an airthmetic expression
+a = b * 2 is an assignment expression */ 
+
+b * 2 // this is an expression statement 
+
+alert(a);
+// this is a call expression statement 
+
+//------------------------------------------------------------------------------------------------//
