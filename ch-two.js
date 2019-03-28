059a6061d7cf720106301f80c1524cfa74d0b0f8
Hannah Huggins <dowdallhannah@yahoo.com>
2019-03-28 10:05:41 +0000

    Started chapter 2, notes on objects

---
+++ ch-two.js
@@ -0,0 +1,21 @@
+// typeof - this can examine a value 
+
+var a = 55;
+
+typeof a; 
+
+//number
+//------------------------------------------------------------------------------------------------//
+
+// Objects 
+
+var obj = {
+    a: "hello world",
+    b: 42,
+    c: true
+}
+
+obj.a; //hello world
+obj.b; // 42
+obj.c; // true
+
