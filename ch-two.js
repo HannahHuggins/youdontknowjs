10e6663b28a75b413c9842da7ccab9fefe5e21e8
Hannah Huggins <dowdallhannah@yahoo.com>
2019-03-28 11:03:49 +0000

    notes on objects and arrays

--- ch-two.js
+++ ch-two.js
@@ -18,4 +18,45 @@ var obj = {
 obj.a; //hello world
 obj.b; // 42
 obj.c; // true
+//dot notation
 
+obj["a"]; // hello world
+obj["b"]; 42
+obj["c"]; true 
+//bracket notation
+
+/* properties can be accessed with either dot notation or bracket notation. 
+Dot notation is generally easier to read, whereas bracket notation is useful if you have a property name 
+that has special characters in it 
+*/
+
+var obj = {
+    a: "hello world",
+    b: 42
+};
+
+var b = "a";
+
+obj[b]; // "hello world"
+obj["b"]; // 42
+
+//------------------------------------------------------------------------------------------------//
+
+// Arrays 
+
+var arr = [
+    "hello world",
+    42,
+    true
+];
+
+arr[0]; // "hello world"
+arr[1]; // 42 
+arr [2]; // true 
+arr.length; // 3
+// length property 
+
+typeof arr; // object 
+
+//languages start counting at 0, 0 is the index of the first element in the array. 
+//------------------------------------------------------------------------------------------------//
